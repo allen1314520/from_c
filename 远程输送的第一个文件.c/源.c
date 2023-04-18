b5394f835b4c90931b4a1729d8be5cf57ae6f88b
allen-anke <3439883617@qq.com>
2023-04-18 23:51:24 +0800

    Allen

---
+++ 远程输送的第一个文件.c/源.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int select(int i)
+{
+	int x = 0;
+	if (i <= 1)
+		return 1;
+	else
+		return i * select(i - 1);
+}
+int main()
+{
+	int n = 0;
+	scanf_s("%d", &n);
+	int ret = 0;
+	ret = select(n);
+	printf("%d\n", ret);
+
+}
