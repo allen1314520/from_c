54606ab3a609c9fc556a283b75889af92bfc036e
allen-anke <3439883617@qq.com>
2023-04-20 21:23:22 +0800

    Allen

---
+++ ++使用test.c/allentest.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+int main()
+{
+    
+    
+    int i;
+        if (i > sizeof(i))
+        {
+            printf(">\n");
+        }
+        else
+        {
+            printf("<\n");
+        }
+        return 0;
+	//int i = 0;
+	//int ret = (++i) + (++i) + (++i);
+	//printf("%d", ret);
+	return 0;
+}
