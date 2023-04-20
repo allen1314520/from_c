a481f85c10257dd4593f9c43eabbbf33e2491e82
allen-anke <3439883617@qq.com>
2023-04-20 22:53:37 +0800

    Allen

---
+++ ascii码显示/allentest.c
@@ -0,0 +1,9 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include <stdio.h>
+
+int main() {
+    int a, b, c, d, e, f, g, h, i, j, k, m;
+    scanf("%d%d%d%d%d%d%d%d%d%d%d%d", &a, &b, &c, &d, &e, &f, &g, &h, &i, &j, &k, &m);
+    printf("%c%c%c%c%c%c%c%c%c%c%c%c", a, b, c, d, e, f, g, h, i, j, k, m);
+    return 0;
+}
